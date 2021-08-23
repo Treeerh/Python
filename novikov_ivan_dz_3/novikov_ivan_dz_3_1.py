47fc32bf3301835cb014c2c01d4874793ef5923f
Treeth <kolisto.07@gmail.com>
2021-08-23 23:30:18 +0300

    add task 1-4

--- novikov_ivan_dz_3/novikov_ivan_dz_3_1.py
+++ novikov_ivan_dz_3/novikov_ivan_dz_3_1.py
@@ -4,4 +4,22 @@
 #       >>> num_translate("eight")
 #       "восемь"
 #   Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию,
-#   необходимую для перевода: какой тип данных выбрать, в теле функции или снаружи.
+#   необходимую для перевода: какой тип данных выбрать, в теле функции или снаружи.\
+
+def num_translate(num):
+    '''
+    the function returns the dictionary value by the key
+    :param num: key value
+    :return: value
+    '''
+    numbers = {'один':'one', 'два':'two',
+               'три':'three', 'четыре':'four',
+               'пять':'five', 'шесть':'six',
+               'семь':'seven', 'восемь':'eight',
+               'девять':'nine', 'десять':'ten'}
+    return numbers.get(num)
+
+my_num = input('Введите число: ')
+print(num_translate(my_num))
+
+
--- novikov_ivan_dz_3/novikov_ivan_dz_3_2.py
+++ novikov_ivan_dz_3/novikov_ivan_dz_3_2.py
@@ -1,6 +1,29 @@
-#   2. * (вместо задачи 1) Доработать предыдущую функцию в num_translate_adv(): реализовать корректную работу
-#   с числительными, начинающимися с заглавной буквы — результат тоже должен быть с заглавной. Например:
+#   2. * (вместо задачи 1) Доработать предыдущую функцию в num_translate_adv():
+#   реализовать корректную работу
+#   с числительными, начинающимися с заглавной буквы — результат тоже
+#   должен быть с заглавной. Например:
 #       >>> num_translate_adv("One")
 #       "Один"
 #       >>> num_translate_adv("two")
 #       "два"
+
+def num_translate_odv(num):
+    '''
+    the function returns the dictionary value by the key
+    :param num: key value
+    :return: value
+    '''
+
+    numbers = {'один':'one', 'два':'two',
+               'три':'three', 'четыре':'four',
+               'пять':'five', 'шесть':'six',
+               'семь':'seven', 'восемь':'eight',
+               'девять':'nine', 'десять':'ten'}
+    if num.istitle():
+        num = num.lower()
+        return numbers.get(num).title()
+    else:
+        return numbers.get(num)
+
+my_num = input('Введите число: ')
+print(num_translate_odv(my_num))
