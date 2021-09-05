333ab9973afe34a1d3cd2819ca96566e4252b738
Treeth <kolisto.07@gmail.com>
2021-09-05 22:47:05 +0300

    task 7 1

---
+++ novikov_ivan_dz_7/task_dz_7_1.py
@@ -0,0 +1,38 @@
+#    1. Написать скрипт, создающий стартер (заготовку) для проекта со следующей структурой папок:
+#    |--my_project
+#       |--settings
+#       |--mainapp
+#       |--adminapp
+#       |--authapp
+#   Примечание: подумайте о ситуации, когда некоторые папки уже есть на диске (как быть?);
+#   как лучше хранить конфигурацию этого стартера, чтобы в будущем можно было менять имена
+#   папок под конкретный проект; можно ли будет при этом расширять конфигурацию и
+#   хранить данные о вложенных папках и файлах (добавлять детали)?
+
+# Влоб
+import os, sys
+import yaml
+# #
+# my_strucs = {'my_project':[{'settings':['vol','main']},'mainapp','adminapp','authapp']}
+#
+# path = os.getcwd() #текущий каталог
+#
+# for key, valls in my_strucs.items():
+#     if os.path.isdir(f'{path}\\{key}'):
+#         sys.exit('Пака существует')
+#     else:
+#         os.mkdir(f'{path}\\{key}')
+#         print(f'Созданна папка {key}')
+#     for idx in valls:
+#         if os.path.isdir(f'{path}\\{key}\\{idx}'):
+#             sys.exit('Пака существует')
+#         else:
+#             os.mkdir(f'{path}\\{key}\\{idx}')
+#             print(f'В папке {key} cозданна папка {idx}')
+# #
+from yaml.loader import SafeLoader
+with open('temp/task_1.yaml','r',encoding='utf-8') as f_struc:
+    r_file = f_struc.read()
+    my_struct = yaml.loader(r_file)
+    print(my_struct)
+
